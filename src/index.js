c20821a46838bad313b3ec2a1dc5f1879cdb7a03
Ayshath_Shaima <ayshathshaima119@gmail.com>
2025-10-30 22:26:05 -0700

    Implemented Skillink Job Portal in collaboration with the development team

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LabourProfile from "./pages/LabourProfile";
-import ManagerProfile from "./pages/Manager/ManagerProfile";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
 <App/>
-{/* <LabourProfile/> */}
-{/* <ManagerProfile/> */}
+
   </BrowserRouter>
 );
